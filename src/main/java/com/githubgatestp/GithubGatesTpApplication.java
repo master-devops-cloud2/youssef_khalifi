71b3d000dfcc363a485c4813a37ffdde319cc88b
youssef-khalifi <youssefkhalifi767@gmail.com>
2024-11-27 11:23:21 +0100

    initial projet

---
+++ src/main/java/com/githubgatestp/GithubGatesTpApplication.java
@@ -0,0 +1,13 @@
+package com.githubgatestp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGatesTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubGatesTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/githubgatestp/GithubGatesTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubgatestp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGatesTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
